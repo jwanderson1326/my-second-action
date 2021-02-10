6e111b7a74e96dc449e12b5d6d7374ffaf6a18d6
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-10 17:17:27 -0500

    just trying hello

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM alpine:latest
 
 RUN apk update && apk add jq
 
-COPY entrypoint.sh /entrypoint.sh
+COPY entrypoint.sh ./entrypoint.sh
 
-ENTRYPOINT [ "/entrypoint.sh" ]
+ENTRYPOINT [ "./entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF == '/refs/heads/main']]
-then
-  export GITCOMMIT=$(cat $GITHUB_EVENT_PATH | jq '.head_commit.id')
-  export GITBRANCH=master
-  echo 'A PUSH TO MASTER!'
-elif [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF != '/refs/heads/main']]
-  echo 'Hello'
-fi
+# if [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF == '/refs/heads/main']]
+# then
+#   export GITCOMMIT=$(cat $GITHUB_EVENT_PATH | jq '.head_commit.id')
+#   export GITBRANCH=master
+#   echo 'A PUSH TO MASTER!'
+# elif [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF != '/refs/heads/main']]
+#   echo 'Hello'
+# fi
+echo "HELLO"
