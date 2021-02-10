4acf1655179f439cb174b3f25b60d73993b42b2c
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-10 17:46:10 -0500

    pull request

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,8 @@ echo $GITHUB_EVENT_NAME
 cat $GITHUB_EVENT_PATH
 
 if [[ $GITHUB_EVENT_NAME == "push" ]]; then
-  export SHA=$(cat $GITHUB_EVENT_PATH | jq '.after')
-  echo $SHA
-  echo $GITHUB_SHA
-  echo $GITHUB_REF
-elif [[ $GITHUB_EVENT_NAME != 'push' ]]; then
+  export SHA=$GITHUB_SHA
+  export REF=$GITHUB_REF
+elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
   echo 'Hello'
 fi
