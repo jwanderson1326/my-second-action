4eeaa776fca15c105a9ea9bbe150199c9ebfaecc
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-10 17:07:46 -0500

    full fledged test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:latest
 
+RUN apk update && apk add jq
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT [ "/entrypoint.sh" ]
--- set_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF == '/refs/heads/main']]
-then
-  export GITCOMMIT=$(cat $GITHUB_EVENT_PATH | jq '.head_commit.id')
-  export GITBRANCH=master
-elif [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF != '/refs/heads/main']]
-  echo 'Hello'
-fi
