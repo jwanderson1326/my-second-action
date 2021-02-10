f3ada74c1ed881ad985cad1e2a944f174719b797
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-10 17:20:57 -0500

    restoring entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# if [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF == '/refs/heads/main']]
-# then
-#   export GITCOMMIT=$(cat $GITHUB_EVENT_PATH | jq '.head_commit.id')
-#   export GITBRANCH=master
-#   echo 'A PUSH TO MASTER!'
-# elif [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF != '/refs/heads/main']]
-#   echo 'Hello'
-# fi
-echo "HELLO"
+if [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF == '/refs/heads/main']]
+then
+  export GITCOMMIT=$(cat $GITHUB_EVENT_PATH | jq '.head_commit.id')
+  export GITBRANCH=master
+  echo 'A PUSH TO MASTER!'
+elif [[ $GITHUB_EVENT_NAME = 'push' ]] && [[ $GITHUB_REF != '/refs/heads/main']]
+  echo 'Hello'
+fi
