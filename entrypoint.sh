f15efc25dd3ae153152aa6ab781bedb717d60756
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-11 12:44:10 -0500

    updating sha pull

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [[ $GITHUB_EVENT_NAME == "push" ]]; then
   export SHA=$GITHUB_SHA
   export REF=$GITHUB_REF
 elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
-  export SHA=$(cat $GITHUB_EVENT_PATH | jq '.head.sha')
+  cat $GITHUB_EVENT_PATH | jq '.head.sha'
   echo $SHA
   echo $GITHUB_SHA
 fi
