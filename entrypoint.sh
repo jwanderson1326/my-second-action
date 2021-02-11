7c976ebfc75e6914077bbacbf305d5973ed937c0
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-11 12:58:48 -0500

    narrowning down

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 echo $GITHUB_EVENT_NAME
-cat $GITHUB_EVENT_PATH
 
 if [[ $GITHUB_EVENT_NAME == "push" ]]; then
   export SHA=$GITHUB_SHA
   export REF=$GITHUB_REF
 elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
-  cat $GITHUB_EVENT_PATH | jq '.head.sha'
+  cat $GITHUB_EVENT_PATH
   echo $GITHUB_SHA
 fi
