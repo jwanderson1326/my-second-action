0954cf73d43d5a0140ae4ae07fac7c077e447cf2
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-11 14:16:38 -0500

    echoing

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
   echo "It's a pull request!"
   export SHA=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.sha')
   export REF=$GITHUB_REF
+  echo $SHA
+  echo $REF
 elif [[ $GITHUB_EVENT_NAME == 'issue_comment' ]]; then
   echo "It's an issue_comment!"
   PR_URL=$(cat $GITHUB_EVENT_PATH | jq -r '.issue.pull_request.url')
