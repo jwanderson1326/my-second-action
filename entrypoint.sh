c2627c739aee9697a3bcc994bf7fbaa2f42fb9be
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-11 13:01:26 -0500

    trying small fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ if [[ $GITHUB_EVENT_NAME == "push" ]]; then
   export SHA=$GITHUB_SHA
   export REF=$GITHUB_REF
 elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
-  cat $GITHUB_EVENT_PATH
+  echo "It's a pull request!"
+  cat $GITHUB_EVENT_PATH | jq '.action'
   echo $GITHUB_SHA
 fi
