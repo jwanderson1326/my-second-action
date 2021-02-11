2a5bbde4c9d9b004917504408497318e0a1ae271
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-11 13:14:30 -0500

    finishing up pr

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ if [[ $GITHUB_EVENT_NAME == "push" ]]; then
   export REF=$GITHUB_REF
 elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
   echo "It's a pull request!"
-  cat $GITHUB_EVENT_PATH | jq '.pull_request.head.sha'
-  echo $GITHUB_SHA
+  export SHA=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.sha')
+  echo $SHA
+  echo $GITHUB_REF
 fi
