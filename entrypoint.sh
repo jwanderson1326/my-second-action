5aeac7875fe939869211782fd1cc570519f52e43
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-11 13:45:44 -0500

    adding some issue comment stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,12 @@ elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
 elif [[ $GITHUB_EVENT_NAME == 'issue_comment' ]]; then
   echo "It's an issue_comment!"
   cat $GITHUB_EVENT_PATH
-  echo $GITHUB_REF
-  echo $GITHUB_SHA
+  PR_URL=$(cat $GITHUB_EVENT_PATH | jq -r '.issue.pull_request.url')
+  echo $PR_URL
+  ISSUE_NUMBER=$(cat $GITHUB_EVENT_PATH | jq -r '.issue.number')
+  export REF="/refs/pull/$ISSUE_NUMBER/merge"
+  echo $REF
+  echo $(curl -i -u jwanderson1326:$INPUT_TOKEN $PR_URL)
   # export SHA=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.sha')
   # export REF=$GITHUB_REF
 fi
