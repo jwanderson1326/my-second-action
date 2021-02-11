0ecc14844db43e3a3e9b42423f55bdea67bf689f
Justin Anderson <justinwarrenanderson@gmail.com>
2021-02-11 13:19:15 -0500

    configuring for issue comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,18 @@
 echo $GITHUB_EVENT_NAME
 
 if [[ $GITHUB_EVENT_NAME == "push" ]]; then
+  echo "It's a push!"
   export SHA=$GITHUB_SHA
   export REF=$GITHUB_REF
 elif [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
   echo "It's a pull request!"
   export SHA=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.sha')
-  echo $SHA
+  export REF=$GITHUB_REF
+elif [[ $GITHUB_EVENT_NAME == 'issue_comment' ]]; then
+  echo "It's an issue_comment!"
+  cat $GITHUB_EVENT_PATH
   echo $GITHUB_REF
+  echo $GITHUB_SHA
+  # export SHA=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.sha')
+  # export REF=$GITHUB_REF
 fi
